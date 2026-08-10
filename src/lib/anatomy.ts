a082452e952eac49f7efe7c8c3226facbfd9e6b5
Abdallah Sabry <abdallahsabry2004@gmail.com>
2026-08-10 18:46:46 +0300

    Create anatomy.ts

---
+++ src/lib/anatomy.ts
@@ -0,0 +1,511 @@
+// src/lib/anatomy.ts
+
+export const ANATOMY_LIBRARY: string[] = [
+
+  // =========================================================
+  // HEAD & FACE
+  // =========================================================
+
+  // Scalp & Facial Expression
+  "Occipitofrontalis",
+  "Frontalis",
+  "Occipitalis",
+  "Temporoparietalis",
+  "Procerus",
+  "Nasalis",
+  "Depressor Septi Nasi",
+  "Levator Labii Superioris Alaeque Nasi",
+  "Levator Labii Superioris",
+  "Zygomaticus Major",
+  "Zygomaticus Minor",
+  "Levator Anguli Oris",
+  "Risorius",
+  "Buccinator",
+  "Orbicularis Oris",
+  "Depressor Anguli Oris",
+  "Depressor Labii Inferioris",
+  "Mentalis",
+  "Platysma",
+
+  // Orbital Muscles
+  "Orbicularis Oculi",
+  "Levator Palpebrae Superioris",
+  "Superior Rectus",
+  "Inferior Rectus",
+  "Medial Rectus",
+  "Lateral Rectus",
+  "Superior Oblique",
+  "Inferior Oblique",
+
+  // Ear
+  "Auricularis Anterior",
+  "Auricularis Superior",
+  "Auricularis Posterior",
+  "Helicis Major",
+  "Helicis Minor",
+  "Tragicus",
+  "Antitragicus",
+  "Transversus Auriculae",
+  "Obliquus Auriculae",
+
+  // Mastication
+  "Masseter",
+  "Temporalis",
+  "Medial Pterygoid",
+  "Lateral Pterygoid",
+
+  // Tongue - Extrinsic
+  "Genioglossus",
+  "Hyoglossus",
+  "Styloglossus",
+  "Palatoglossus",
+
+  // Tongue - Intrinsic
+  "Superior Longitudinal Muscle of Tongue",
+  "Inferior Longitudinal Muscle of Tongue",
+  "Transverse Muscle of Tongue",
+  "Vertical Muscle of Tongue",
+
+  // Soft Palate
+  "Tensor Veli Palatini",
+  "Levator Veli Palatini",
+  "Musculus Uvulae",
+  "Palatopharyngeus",
+
+  // Pharynx
+  "Superior Pharyngeal Constrictor",
+  "Middle Pharyngeal Constrictor",
+  "Inferior Pharyngeal Constrictor",
+  "Stylopharyngeus",
+  "Salpingopharyngeus",
+
+  // Larynx - Intrinsic
+  "Cricothyroid",
+  "Posterior Cricoarytenoid",
+  "Lateral Cricoarytenoid",
+  "Transverse Arytenoid",
+  "Oblique Arytenoid",
+  "Thyroarytenoid",
+  "Vocalis",
+  "Aryepiglottic Muscle",
+  "Thyroepiglottic Muscle",
+
+  // =========================================================
+  // NECK
+  // =========================================================
+
+  // Superficial
+  "Sternocleidomastoid",
+
+  // Suprahyoid
+  "Digastric",
+  "Mylohyoid",
+  "Geniohyoid",
+  "Stylohyoid",
+
+  // Infrahyoid
+  "Sternohyoid",
+  "Omohyoid",
+  "Sternothyroid",
+  "Thyrohyoid",
+
+  // Prevertebral
+  "Longus Capitis",
+  "Longus Colli",
+  "Rectus Capitis Anterior",
+  "Rectus Capitis Lateralis",
+
+  // Scalene
+  "Anterior Scalene",
+  "Middle Scalene",
+  "Posterior Scalene",
+  "Scalenus Minimus",
+
+  // Posterior Neck
+  "Rectus Capitis Posterior Major",
+  "Rectus Capitis Posterior Minor",
+  "Obliquus Capitis Superior",
+  "Obliquus Capitis Inferior",
+
+  // Splenius
+  "Splenius Capitis",
+  "Splenius Cervicis",
+
+  // =========================================================
+  // BACK
+  // =========================================================
+
+  // Superficial Back
+  "Trapezius",
+  "Latissimus Dorsi",
+  "Rhomboid Major",
+  "Rhomboid Minor",
+  "Levator Scapulae",
+
+  // Intermediate Back
+  "Serratus Posterior Superior",
+  "Serratus Posterior Inferior",
+
+  // Erector Spinae
+  "Iliocostalis Cervicis",
+  "Iliocostalis Thoracis",
+  "Iliocostalis Lumborum",
+
+  "Longissimus Capitis",
+  "Longissimus Cervicis",
+  "Longissimus Thoracis",
+
+  "Spinalis Capitis",
+  "Spinalis Cervicis",
+  "Spinalis Thoracis",
+
+  // Transversospinalis
+  "Semispinalis Capitis",
+  "Semispinalis Cervicis",
+  "Semispinalis Thoracis",
+
+  "Multifidus",
+  "Rotatores Breves",
+  "Rotatores Longi",
+
+  // Deep Back
+  "Interspinales",
+  "Intertransversarii",
+  "Levatores Costarum",
+
+  // =========================================================
+  // THORAX
+  // =========================================================
+
+  "Pectoralis Major",
+  "Pectoralis Minor",
+  "Subclavius",
+  "Serratus Anterior",
+
+  // Intercostals
+  "External Intercostal",
+  "Internal Intercostal",
+  "Innermost Intercostal",
+  "Subcostalis",
+  "Transversus Thoracis",
+
+  // Respiratory
+  "Diaphragm",
+
+  // =========================================================
+  // ABDOMEN
+  // =========================================================
+
+  "Rectus Abdominis",
+  "Pyramidalis",
+  "External Oblique",
+  "Internal Oblique",
+  "Transversus Abdominis",
+  "Quadratus Lumborum",
+
+  // =========================================================
+  // PELVIS
+  // =========================================================
+
+  // Pelvic Floor
+  "Levator Ani",
+  "Puborectalis",
+  "Pubococcygeus",
+  "Iliococcygeus",
+  "Coccygeus",
+
+  // Perineum
+  "External Anal Sphincter",
+  "External Urethral Sphincter",
+  "Bulbospongiosus",
+  "Ischiocavernosus",
+  "Superficial Transverse Perineal",
+  "Deep Transverse Perineal",
+
+  // =========================================================
+  // SHOULDER / SCAPULAR REGION
+  // =========================================================
+
+  "Deltoid",
+
+  // Rotator Cuff
+  "Supraspinatus",
+  "Infraspinatus",
+  "Teres Minor",
+  "Subscapularis",
+
+  "Teres Major",
+
+  // =========================================================
+  // ARM
+  // =========================================================
+
+  // Anterior Compartment
+  "Biceps Brachii",
+  "Coracobrachialis",
+  "Brachialis",
+
+  // Posterior Compartment
+  "Triceps Brachii",
+  "Anconeus",
+
+  // =========================================================
+  // FOREARM - ANTERIOR
+  // =========================================================
+
+  // Superficial
+  "Pronator Teres",
+  "Flexor Carpi Radialis",
+  "Palmaris Longus",
+  "Flexor Carpi Ulnaris",
+
+  // Intermediate
+  "Flexor Digitorum Superficialis",
+
+  // Deep
+  "Flexor Digitorum Profundus",
+  "Flexor Pollicis Longus",
+  "Pronator Quadratus",
+
+  // =========================================================
+  // FOREARM - POSTERIOR / LATERAL
+  // =========================================================
+
+  // Lateral
+  "Brachioradialis",
+  "Extensor Carpi Radialis Longus",
+  "Extensor Carpi Radialis Brevis",
+
+  // Superficial Posterior
+  "Extensor Digitorum",
+  "Extensor Digiti Minimi",
+  "Extensor Carpi Ulnaris",
+
+  // Deep Posterior
+  "Supinator",
+  "Abductor Pollicis Longus",
+  "Extensor Pollicis Brevis",
+  "Extensor Pollicis Longus",
+  "Extensor Indicis",
+
+  // =========================================================
+  // HAND
+  // =========================================================
+
+  // Thenar
+  "Abductor Pollicis Brevis",
+  "Flexor Pollicis Brevis",
+  "Opponens Pollicis",
+  "Adductor Pollicis",
+
+  // Hypothenar
+  "Abductor Digiti Minimi",
+  "Flexor Digiti Minimi Brevis",
+  "Opponens Digiti Minimi",
+  "Palmaris Brevis",
+
+  // Central
+  "Lumbricals of Hand",
+  "Dorsal Interossei of Hand",
+  "Palmar Interossei of Hand",
+
+  // =========================================================
+  // HIP / GLUTEAL REGION
+  // =========================================================
+
+  "Gluteus Maximus",
+  "Gluteus Medius",
+  "Gluteus Minimus",
+  "Tensor Fasciae Latae",
+
+  // Deep External Rotators
+  "Piriformis",
+  "Obturator Internus",
+  "Superior Gemellus",
+  "Inferior Gemellus",
+  "Quadratus Femoris",
+  "Obturator Externus",
+
+  // =========================================================
+  // THIGH - ANTERIOR
+  // =========================================================
+
+  "Sartorius",
+
+  // Quadriceps
+  "Rectus Femoris",
+  "Vastus Lateralis",
+  "Vastus Medialis",
+  "Vastus Intermedius",
+
+  "Articularis Genu",
+
+  // =========================================================
+  // THIGH - MEDIAL
+  // =========================================================
+
+  "Pectineus",
+  "Adductor Longus",
+  "Adductor Brevis",
+  "Adductor Magnus",
+  "Gracilis",
+
+  // =========================================================
+  // THIGH - POSTERIOR
+  // =========================================================
+
+  "Biceps Femoris",
+  "Biceps Femoris Long Head",
+  "Biceps Femoris Short Head",
+  "Semitendinosus",
+  "Semimembranosus",
+
+  // =========================================================
+  // LEG - ANTERIOR
+  // =========================================================
+
+  "Tibialis Anterior",
+  "Extensor Hallucis Longus",
+  "Extensor Digitorum Longus",
+  "Fibularis Tertius",
+
+  // =========================================================
+  // LEG - LATERAL
+  // =========================================================
+
+  "Fibularis Longus",
+  "Fibularis Brevis",
+
+  // =========================================================
+  // LEG - POSTERIOR SUPERFICIAL
+  // =========================================================
+
+  "Gastrocnemius",
+  "Soleus",
+  "Plantaris",
+
+  // =========================================================
+  // LEG - POSTERIOR DEEP
+  // =========================================================
+
+  "Popliteus",
+  "Tibialis Posterior",
+  "Flexor Digitorum Longus",
+  "Flexor Hallucis Longus",
+
+  // =========================================================
+  // FOOT - DORSAL
+  // =========================================================
+
+  "Extensor Digitorum Brevis",
+  "Extensor Hallucis Brevis",
+
+  // =========================================================
+  // FOOT - PLANTAR LAYER 1
+  // =========================================================
+
+  "Abductor Hallucis",
+  "Flexor Digitorum Brevis",
+
+  // =========================================================
+  // FOOT - PLANTAR LAYER 2
+  // =========================================================
+
+  "Quadratus Plantae",
+  "Lumbricals of Foot",
+
+  // =========================================================
+  // FOOT - PLANTAR LAYER 3
+  // =========================================================
+
+  "Flexor Hallucis Brevis",
+  "Adductor Hallucis",
+
+  // =========================================================
+  // FOOT - PLANTAR LAYER 4
+  // =========================================================
+
+  "Dorsal Interossei of Foot",
+  "Plantar Interossei of Foot",
+
+  // =========================================================
+  // ADDITIONAL SMALL / VARIANT MUSCLES
+  // =========================================================
+
+  "Iliopsoas",
+
+  // =========================================================
+  // HIP FLEXOR / POSTERIOR ABDOMINAL WALL
+  // =========================================================
+
+  "Psoas Major",
+  "Psoas Minor",
+  "Iliacus",
+
+  // =========================================================
+  // MUSCLES OF THE MIDDLE EAR
+  // =========================================================
+
+  "Stapedius",
+  "Tensor Tympani",
+
+  // =========================================================
+  // ADDITIONAL PHARYNGEAL / LARYNGEAL STRUCTURES
+  // =========================================================
+
+  "Cricopharyngeus",
+  "Aryepiglotticus",
+
+  // =========================================================
+  // ADDITIONAL SPINAL SEGMENTAL MUSCLES
+  // =========================================================
+
+  "Intertransversarii Cervicis",
+  "Intertransversarii Thoracis",
+  "Intertransversarii Lumborum",
+  "Interspinales Cervicis",
+  "Interspinales Thoracis",
+  "Interspinales Lumborum",
+
+  // =========================================================
+  // ADDITIONAL MUSCLES / ANATOMICAL VARIANTS
+  // =========================================================
+
+  "Sternalis",
+  "Levator Costarum",
+
+  // =========================================================
+  // CARDIAC MUSCLE
+  // =========================================================
+
+  // Included only as anatomical reference.
+  // It is NOT a skeletal muscle.
+  "Myocardium",
+
+  // =========================================================
+  // GENERAL / MUSCLE GROUP LABELS
+  // =========================================================
+
+  // These are useful for clinical assessment,
+  // but they are muscle groups rather than individual muscles.
+
+  "Quadriceps Femoris",
+  "Hamstrings",
+  "Rotator Cuff",
+  "Erector Spinae",
+  "Pelvic Floor Muscles",
+  "Hip Flexors",
+  "Hip Adductors",
+  "Hip Abductors",
+  "Hip External Rotators",
+  "Hip Internal Rotators",
+  "Wrist Flexors",
+  "Wrist Extensors",
+  "Finger Flexors",
+  "Finger Extensors",
+  "Intrinsic Hand Muscles",
+  "Intrinsic Foot Muscles",
+  "Calf Muscles",
+  "Abdominal Muscles",
+  "Intercostal Muscles",
+];
