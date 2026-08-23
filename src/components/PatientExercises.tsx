2865233e2af7075bc7c6c6739ab4ee14cd98ddc8
Abdallah Sabry <abdallahsabry2004@gmail.com>
2026-08-23 14:24:46 +0300

    Update PatientExercises.tsx

--- src/components/PatientExercises.tsx
+++ src/components/PatientExercises.tsx
@@ -264,7 +264,7 @@ export function PatientExercises({ patientId }: { patientId: string }) {
                 <p className="text-gray-500 mt-2 font-medium">
                   Date: {format(new Date(), "dd/MM/yyyy hh:mm a")}
                 </p>
-                <p className="text-gray-500 font-medium">Therapist: {fullName}</p>
+                <p className="text-gray-500 font-medium">Exported by: {fullName}</p>
               </div>
             </div>
 
